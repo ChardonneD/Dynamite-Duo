0c3d0457d4fb085d8a90eaefc9074ad30ba8bf93
Chardonne Dixon <chardonne.dixon@gmail.com>
2026-03-07 11:09:45 -0600

    update

--- .history/scripts/main_20260307110308.js
+++ .history/scripts/main_20260307110308.js
@@ -22,5 +22,5 @@ debugger
     This is the code you wrote in the last chapter to get a
     reference to the <main> element
 */
-const theExistingMainElement = document.querySelector("container")
+const theExistingMainElement = document.querySelector("#container")
 theExistingMainElement.innerHTML = theWholeEnchilada
